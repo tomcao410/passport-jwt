a64b7786a902b15df7afc178ddb0b5ef699d9d65
Tom Cao <itom41098@gmail.com>
2019-10-24 19:24:05 +0700

    Update CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 var passport = require('passport');
 require('./auth/passport');
 
@@ -12,6 +13,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.options('*', cors());
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,6 +28,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 app.use('/users', passport.authenticate('jwt', { session : false }), usersRouter );
+
+// app.use(cors({ origin: 'http://localhost:3001' , credentials :  false}));
+// app.options('*', cors( { origin: '*' , credentials : true } ));
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+  next();
+}
+
+app.use(allowCrossDomain); // plumbing it in as middleware
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,5 +55,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
